ddd3e8f6d9e93f036cb8b7ef3f1e480eb3eb1f31
jiebinzhou98 <benzhou98@outlook.com>
2025-04-14 21:39:45 -0400

    fixing deploy problem

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,18 +6,18 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      },
-      '/graphql': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:5000',
+    //     changeOrigin: true,
+    //     secure: false,
+    //     rewrite: (path) => path.replace(/^\/api/, '')
+    //   },
+    //   '/graphql': {
+    //     target: 'http://localhost:5000',
+    //     changeOrigin: true,
+    //     secure: false,
+    //   }
+    // }
   },
 })
